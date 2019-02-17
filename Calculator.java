594ce5239b671b08e6d166cc8af3b3fcb8ef9599
nakhan8 <nakhan8@asu.edu>
2019-02-16 19:45:06 -0700

    Initial Version

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+package cse360assign3;
+
+ /** Calculator Program
+ * @author Nabeel Asif
+ * @version February 16, 2019
+ */
+public class Calculator {
+
+ 	private int total;
+	/**
+	 * Create a Calculator object and set to total value to 0
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * When you call this function the total will be returned.
+	 * Right now it will just return 0.
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * add will simply add the value that is inputed in the method signature
+	 * @param value - the value to be added
+	 */
+	public void add (int value) {
+
+ 	}
+	/**
+	 * subtract will simply subtract the value that is inputed in the method signature
+	 * @param value - the value to be subtracted
+	 */
+	public void subtract (int value) {
+
+ 	}
+	/**
+	 * multiply will simply multiply the value that is inputed in the method signature.
+	 * @param value - the value to be multiplied
+	 */
+	public void multiply (int value) {
+
+ 	}
+	/**
+	 * divide will simply divide the value that is inputed in the method signature.
+	 * @param value - the value to be divided
+	 */
+	public void divide (int value) {
+
+ 	}
+	/**
+	 * getHistory will return the calculation history that was done previously.
+	 * @return will return the calculation history.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
